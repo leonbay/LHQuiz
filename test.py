8a65ff9e22b3091da0ff79ac41c22a1a4cbd6583
henri <heneneri@gmail.com>
2021-07-06 10:28:05 +0300

    runko

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-test
+score = 0
+def alku():
+    alotus = input('haluatko pelata')
+    return True
+
+def kysymys1():
+    while alotus == True:
+        if True:
+            score += 1
+
+def tulos():
+    print(f'Sait oikein: {score}')
