48c52ca3a2a594fc0c474ae57e2665967ba2a519
LahenLeo <lahtileo.o@gmail.com>
2021-07-06 10:45:31 +0300

    leon muokkaukset

--- test.py
+++ test.py
@@ -1,12 +1,10 @@
 score = 0
 def alku():
-    alotus = input('haluatko pelata')
-    return True
+    aloitus = input("Haluatko pelata? (y/n)")
+    if aloitus == y:
+        return True
+    elif aloitus == n:
 
-def kysymys1():
-    while alotus == True:
-        if True:
-            score += 1
-
-def tulos():
-    print(f'Sait oikein: {score}')
+        
+#def tulos():
+    #print(f'Sait oikein: {score}')
