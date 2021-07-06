fcfbd88c9efa74d96f96938062e6374b935d1dc1
henri <heneneri@gmail.com>
2021-07-06 11:05:30 +0300

    kysymykset

--- quiz.py
+++ quiz.py
@@ -1,12 +1,31 @@
 score = 0
-def alku():
-    alotus = input('haluatko pelata')
-    return True
 
 def kysymys1():
-    while alotus == True:
-        if True:
-            score += 1
+    while aloitus == True:
+        vastaus1 = input('Mikä maa voittaa jalkapallon Euroopan-mestarruden vuonna 2021? \na. Suomi \nb. Italia \nc. Tanska \nd. Kanada \nVastaus: ')
+        if vastaus1 == 'b':
+            Score += 1
+            print('Oikein!')
+        else:
+            print('Väärin! Oikea vastaus oli Italia')
+    else:
+        break
+
+def kysymys2():
+    while aloitus == True:
+        vastaus1 = input('Mikä on paras pilvipalvelu? \na. AWS \nb. Azure \nc. Gcloud \nVastaus: ')
+        if vastaus1 == 'c':
+            Score += 1
+            print('Oikein!')
+        else:
+            print('Väärin! Oikea vastaus oli Italia')
+    else:
+        break
 
 def tulos():
     print(f'Sait oikein: {score}')
+
+alku()
+kysymys1()
+kysymys2()
+tulos()
